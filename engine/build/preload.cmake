75e6fcb1b51a7706e39f50bd2ac9002ebbfcfefb
David Knapp <mazecraze96@gmail.com>
2019-12-03 14:12:10 -0500

    CMake: Delete preload.cmake. Wasn't really needed in the first place.

--- engine/build/preload.cmake
+++
@@ -1,5 +0,0 @@
-if(PROJ_DIR STREQUAL "")
-	message(FATAL "No PROJ_DIR specified. Specify -DPROJ_DIR to the the directory containing your build config. If you got here from the build script, it's broken again.")
-endif()
-
-include("${PROJ_DIR}/config.cmake")
