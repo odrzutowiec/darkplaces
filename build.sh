746b7a3ebb83646b724406a608684ed4eb7f4d8c
Cloudwalk <mazecraze96@gmail.com>
2020-01-18 14:12:21 -0500

    Misc: Update and beautify copyright headers

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-
-#---------------------------------------------------------------#
-# FILE: build.sh (Horsepower/Darkplaces Build Script)           #
-# AUTHOR: David Knapp (Cloudwalk)                               #
-# LICENSE: GPL2.0 or later                                      #
-# Copyright (C) 2019-2020 David Knapp                                #
-#---------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#   build.sh (Horsepower/Darkplaces Build Script)                              #
+#                                                                              #
+#   Copyright (c) 2020 Cloudwalk                                               #
+#                                                                              #
+#   This program is free software: you can redistribute it and/or modify       #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.      #
+#------------------------------------------------------------------------------#
 if [ -z "${BASH_VERSION:-}" ]; then
 	echo "This script requires bash."
 	exit 1
