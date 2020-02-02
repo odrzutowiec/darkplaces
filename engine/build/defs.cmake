d6b7cf6c0cca7fbb4e40f8327ce63ab5de9c304f
David Knapp <mazecraze96@gmail.com>
2020-02-02 09:09:28 -0500

    cmake: Don't use .tmp for library prefix. It can be in the build directory instead.
    Besides, there's already ".temp".

--- engine/build/defs.cmake
+++ engine/build/defs.cmake
@@ -10,7 +10,7 @@ set(CM_DIR "${HP_DIR}/build")				# Build system scripts
 set(UTIL_DIR "${CM_DIR}/utility")			# Utility libraries for the build system
 set(LIB_DIR "${CM_DIR}/libs")				# Find dependency libraries for the engine
 set(TGT_DIR "${CM_DIR}/target")				# Scripts to build the engine itself
-set(TOOLCHAIN_DIR "${CM_DIR}/toolchain")		# Toolchain files for cross-compiling
+set(TOOLCHAIN_DIR "${CM_DIR}/toolchain")	# Toolchain files for cross-compiling
 set(PROJ_DIR "${ROOT_DIR}/game")			# Default location for game projects
 set(PROJ_DEFAULT_DIR "${PROJ_DIR}/default")	# Location of the default template
-set(TMP_DIR "${ROOT_DIR}/.tmp")				# Download and unpack files here
+set(TMP_DIR "${ROOT_DIR}/.temp")			# General purpose temp directory
